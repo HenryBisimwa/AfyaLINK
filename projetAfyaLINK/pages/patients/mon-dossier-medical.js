ccfb72c434796b9c0c3c2ec8e94cdb0f5302b18d
Bédieu <henrybisimwa591@gmail.com>
2026-07-20 16:48:16 +0200

    V.9 page mon dossier medical

---
+++ projetAfyaLINK/pages/patients/mon-dossier-medical.js
@@ -0,0 +1,7 @@
+const bouton = document.querySelector("#telecharger");
+
+bouton.addEventListener("click", function () {
+
+    alert("Le téléchargement du dossier médical sera bientôt disponible.");
+
+});
